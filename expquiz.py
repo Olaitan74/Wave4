fdb390f7dc0a8e97f3bdb680c74c5510d024aeba
Olaitan <55138894+Olaitan74@users.noreply.github.com>
2020-08-16 15:36:35 +0100

    Scripting solutions

---
+++ expquiz.py
@@ -0,0 +1,8 @@
+#It's your turn to import and use the math module. Use the math module to calculate e to the power of 3. print the answer.
+# print e to the power of 3 using the math module
+
+import math
+
+exp_of_3 = math.exp(3)
+print(exp_of_3)
+
---
+++ match_flower_name.py
@@ -0,0 +1,33 @@
+#For the following practice question you will need to write code in Python in the workspace below. This will allow you to practice the concepts discussed in the Scripting lesson, such as reading and writing files. You will see some older concepts too, but again, we have them there to review and reinforce your understanding of those concepts.
+
+#Question: Create a function that opens the flowers.txt, reads every line in it, and saves it as a dictionary. The main (separate) function should take user input (user's first name and last name) and parse the user input to identify the first letter of the first name. It should then use it to print the flower name with the same first letter (from dictionary created in the first function).
+
+#Sample Output:
+
+#>>> Enter your First [space] Last name only: Bill Newman
+#>>> Unique flower name with the first letter: Bellflower
+
+# Write your code here
+
+# HINT: create a dictionary from flowers.txt
+
+# HINT: create a function
+def create_flowerdict(filename):
+    flower_dict = {}
+    with open(filename) as f:
+        for flower_name in f:
+            letter = flower_name.split(": ")[0].lower()
+            flower = flower_name.split(": ")[1].strip()
+            flower_dict[letter] = flower
+    return flower_dict
+
+
+def main():
+    flower_d = create_flowerdict('flowers.txt')
+    full_name = input("Enter your First [space] Last name only: ")
+    first_name = full_name[0].lower()
+    first_letter = first_name[0]
+# print command that prints final input with value from corresponding key in dictionary
+    print("Unique flower name with the first letter: {}".format(flower_d[first_letter]))
+
+main()
